938103a0d16d55b9f636fc2c31d3e7f503e16836
Diogo Cardoso <diogocardoso92@outlook.com>
2022-08-10 14:09:55 +0100

    Time: 165 ms (33.94%), Space: 42.3 MB (81.60%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -3,6 +3,8 @@
  * @param {number} target
  * @return {number[]}
  */
+
+// The inner/2nd loop doesn't have to run across the entire array, because we would be repeating combinations. Note that a + b = b + a.
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
