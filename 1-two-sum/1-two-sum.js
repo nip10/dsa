8cbef64d8ea98bba6fb2fc76d8b4f6a64ae57e19
Diogo Cardoso <diogocardoso92@outlook.com>
2022-08-10 14:19:51 +0100

    Time: 89 ms (81.50%), Space: 42.7 MB (52.87%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -5,6 +5,7 @@
  */
 
 // The inner/2nd loop doesn't have to run across the entire array, because we would be repeating combinations. Note that a + b = b + a.
+/*
 var twoSum = function(nums, target) {
     for (let i = 0; i < nums.length; i++) {
         for (let j = i + 1; j < nums.length; j++) {
@@ -14,3 +15,19 @@ var twoSum = function(nums, target) {
         }
     }
 };
+*/
+
+var twoSum = function(nums, target) {
+    const hashtable = {};
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+       
+        if (hashtable.hasOwnProperty(complement)){
+          return [hashtable[complement], i]
+        }
+ 
+        hashtable[nums[i]] = i;   
+     };
+};
+
+     
