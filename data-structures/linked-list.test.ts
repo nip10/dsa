c1cad0166aec68fe6d8597f27a88ff53f9cd7987
Diogo Cardoso <diogo.cardoso@replai.io>
2022-08-11 18:26:55 +0100

    add linked list

---
+++ data-structures/linked-list.test.ts
@@ -0,0 +1,244 @@
+import util from "util";
+import { assert, beforeEach, describe, expect, it } from "vitest";
+import { LinkedList, ListNode } from "./linked-list";
+
+describe("init", () => {
+  it("shoud create a node", () => {
+    const node = new ListNode("foo");
+    expect(node.value).toBe("foo");
+  });
+
+  it("shoud create a list with a single node", () => {
+    const node = new ListNode("foo");
+    const list = new LinkedList(node);
+    expect(list.head).toBe(node);
+  });
+
+  it("shoud create a list with a multiple nodes", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+    expect(list.head).toBe(node1);
+    expect(list.head!.next).toBe(node2);
+    expect(list.head!.next!.next).toBe(node3);
+  });
+});
+
+describe("append", () => {
+  it("shoud append to list", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+
+    const node = new ListNode("newnode");
+    list.append(node);
+    expect(list.head?.next?.next?.next).toBe(node);
+  });
+
+  it("shoud append to empty list", () => {
+    const list = new LinkedList();
+    const node = new ListNode("newnode");
+    list.append(node);
+    expect(list.head).toBe(node);
+  });
+});
+
+describe("prepend", () => {
+  it("shoud prepend to list", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+
+    const node = new ListNode("newnode");
+    list.prepend(node);
+    expect(list.head).toBe(node);
+    expect(list.head?.next).toBe(node1);
+  });
+
+  it("shoud prepend to empty list", () => {
+    const list = new LinkedList();
+    const node = new ListNode("newnode");
+    list.prepend(node);
+    expect(list.head).toBe(node);
+  });
+});
+
+describe("size", () => {
+  it("shoud return 0 on empty list", () => {
+    const list = new LinkedList();
+    const size = list.size();
+    expect(size).toBe(0);
+  });
+
+  it("shoud return size on list with multiple nodes", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+    const size = list.size();
+    expect(size).toBe(3);
+  });
+});
+
+describe("clear", () => {
+  it("should work on empty list", () => {
+    const list = new LinkedList();
+    list.clear();
+    expect(list.head).toBeUndefined();
+  });
+
+  it("should work on list with multiple nodes", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+    list.clear();
+    expect(list.head).toBeUndefined();
+  });
+});
+
+describe("get head", () => {
+  it("should return undefined when list is empty", () => {
+    const list = new LinkedList();
+    const head = list.getHead();
+    expect(head).toBeUndefined();
+  });
+
+  it("should return head element on list with multiple nodes", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+    const head = list.getHead();
+    expect(head).toBe(node1);
+  });
+});
+
+describe("get tail", () => {
+  it("should return undefined when list is empty", () => {
+    const list = new LinkedList();
+    const tail = list.getTail();
+    expect(tail).toBeUndefined();
+  });
+
+  it("should return tail element on list with multiple nodes", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+    const tail = list.getTail();
+    expect(tail).toBe(node3);
+  });
+});
+
+describe("remove first", () => {
+  it("should not crash when list is empty", () => {
+    const list = new LinkedList();
+    list.removeFirst();
+    expect(list.head).toBeUndefined();
+  });
+
+  it("should remove first element on list with multiple nodes", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+    list.removeFirst();
+    expect(list.head).toBe(node2);
+  });
+});
+
+describe("remove last", () => {
+  it("should not crash when list is empty", () => {
+    const list = new LinkedList();
+    list.removeFirst();
+    expect(list.head).toBeUndefined();
+  });
+
+  it("should remove last element on list with multiple nodes", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+    list.removeLast();
+    expect(list.head?.next?.next).toBeUndefined();
+  });
+});
+
+describe("remove at", () => {
+  it("should not crash when index is negative", () => {
+    const list = new LinkedList();
+    list.removeAt(-23);
+    expect(list.head).toBeUndefined();
+  });
+
+  it("should not crash when index is greater than list size", () => {
+    const list = new LinkedList();
+    list.removeAt(23);
+    expect(list.head).toBeUndefined();
+  });
+
+  it("should remove element on specified index", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+    list.removeAt(1);
+    expect(list.head?.next).toBe(node3);
+  });
+});
+
+describe("insert at", () => {
+  it("should not crash when index is negative", () => {
+    const list = new LinkedList();
+    const node = new ListNode("newnode");
+    list.insertAt(-23, node);
+    expect(list.head).toBeUndefined();
+  });
+
+  it("should not crash when index is greater than list size", () => {
+    const list = new LinkedList();
+    const node = new ListNode("newnode");
+    list.insertAt(23, node);
+    expect(list.head).toBeUndefined();
+  });
+
+  it("should insert element on specified index", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+
+    const newNode = new ListNode("newnode");
+    list.insertAt(1, newNode);
+    expect(list.head?.next).toBe(newNode);
+    expect(list.head?.next?.next).toBe(node2);
+  });
+});
+
+describe("get at", () => {
+  it("should not crash when index is negative", () => {
+    const list = new LinkedList();
+    const node = list.getAt(-23);
+    expect(node).toBeUndefined();
+  });
+
+  it("should not crash when index is greater than list size", () => {
+    const list = new LinkedList();
+    const node = list.getAt(23);
+    expect(node).toBeUndefined();
+  });
+
+  it("should return node", () => {
+    const node3 = new ListNode("baz");
+    const node2 = new ListNode("bar", node3);
+    const node1 = new ListNode("foo", node2);
+    const list = new LinkedList(node1);
+
+    const node = list.getAt(1);
+    expect(node).toBe(node2);
+  });
+});
---
+++ data-structures/linked-list.ts
@@ -0,0 +1,128 @@
+export class ListNode {
+  value: string | number;
+  next: ListNode | undefined;
+
+  constructor(val: string | number, next?: ListNode) {
+    this.value = val;
+    this.next = next;
+  }
+}
+
+export class LinkedList {
+  head: ListNode | undefined = undefined;
+
+  constructor(head?: ListNode) {
+    this.head = head;
+  }
+
+  append(node: ListNode): void {
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+    tail.next = node;
+  }
+
+  prepend(node: ListNode): void {
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+    node.next = this.head;
+    this.head = node;
+  }
+
+  size(): number {
+    let size = 0;
+    let curNode = this.head;
+    while (curNode) {
+      size++;
+      curNode = curNode.next;
+    }
+    return size;
+  }
+
+  clear(): void {
+    this.head = undefined;
+  }
+
+  getHead(): ListNode | undefined {
+    return this.head;
+  }
+
+  getTail(): ListNode | undefined {
+    if (!this.head) return undefined;
+    let tail = this.head;
+    while (tail.next) {
+      tail = tail.next;
+    }
+    return tail;
+  }
+
+  removeFirst(): void {
+    if (!this.head) return;
+    this.head = this.head.next;
+  }
+
+  removeLast(): void {
+    if (!this.head) return;
+    let tail = this.head.next;
+    if (!tail) {
+      this.head = undefined;
+      return;
+    }
+    let previous = this.head;
+    while (tail.next) {
+      previous = tail;
+      tail = tail.next;
+    }
+    previous.next = undefined;
+  }
+
+  removeAt(idx: number): void {
+    let iter = 0;
+    let curNode = this.head;
+    if (!curNode) return;
+    while (curNode.next && iter < idx) {
+      if (iter === idx - 1) {
+        curNode.next = curNode.next.next;
+        return;
+      }
+      curNode = curNode.next;
+      iter++;
+    }
+  }
+
+  insertAt(idx: number, node: ListNode): void {
+    let iter = 0;
+    let curNode = this.head;
+    if (!curNode) return;
+    while (curNode.next && iter < idx) {
+      if (iter === idx - 1) {
+        node.next = curNode.next;
+        curNode.next = node;
+        return;
+      }
+      curNode = curNode.next;
+      iter++;
+    }
+  }
+
+  getAt(idx: number): ListNode | undefined {
+    let iter = 0;
+    let curNode = this.head;
+    if (!curNode) return undefined;
+    while (curNode.next && iter <= idx) {
+      if (iter === idx) {
+        return curNode;
+      }
+      curNode = curNode.next;
+      iter++;
+    }
+    return undefined;
+  }
+}
