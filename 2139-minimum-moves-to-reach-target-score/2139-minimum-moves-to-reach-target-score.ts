bad5f9169b2eba5aeddb09a592c96b9e66757c76
Diogo Cardoso <diogocardoso92@outlook.com>
2022-08-12 00:22:29 +0100

    Time: 120 ms (20.00%), Space: 42.7 MB (90.00%) - LeetHub

---
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.ts
@@ -0,0 +1,25 @@
+function minMoves(target: number, maxDoubles: number): number {
+    if (target === 1) return 0;
+    let ans = 0;
+    while (maxDoubles !== 0) {
+        const a = target / 2;
+        // Can't be negative or we wouldn't be able to use a double
+        if (a > 0) {
+            // if is decimal, we need one extra increment (and remember the double)
+            if (a % 1 !== 0) {
+                ans += 2;
+                target = Math.floor(a);
+            } else {
+                ans++;
+                target = a;
+            }
+            if (target === 1) break;
+        }
+        maxDoubles--;
+    }
+    ans += target !== 1 ? target - 1 : 0;
+    return ans;
+};
+
+
+
