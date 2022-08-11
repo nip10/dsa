004d680a77b035c8aa4b25d583ad973a9d09f041
Diogo Cardoso <diogocardoso92@outlook.com>
2022-08-12 00:23:29 +0100

    Time: 130 ms (10.00%), Space: 43.4 MB (30.00%) - LeetHub

--- 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.ts
+++ 2139-minimum-moves-to-reach-target-score/2139-minimum-moves-to-reach-target-score.ts
@@ -17,7 +17,9 @@ function minMoves(target: number, maxDoubles: number): number {
         }
         maxDoubles--;
     }
-    ans += target !== 1 ? target - 1 : 0;
+    if (target !== 1) {
+        ans += target - 1;
+    }
     return ans;
 };
 
