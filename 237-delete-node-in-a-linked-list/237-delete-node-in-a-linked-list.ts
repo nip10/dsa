ecde9d26d3a8fc594693ddc988ff41420fce365b
Diogo Cardoso <diogo.cardoso@replai.io>
2022-08-11 18:26:42 +0100

    add missing class

--- 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.ts
+++ 237-delete-node-in-a-linked-list/237-delete-node-in-a-linked-list.ts
@@ -9,11 +9,24 @@
  *     }
  * }
  */
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
 
 /**
  Do not return anything, modify it in-place instead.
  */
 function deleteNode(root: ListNode | null): void {
-    root.val = root.next.val;
-    root.next = root.next.next;
-};
+  if (root === null) return;
+  if (root.next === null) {
+    root = null;
+    return;
+  }
+  root.val = root.next.val;
+  root.next = root.next.next;
+}
