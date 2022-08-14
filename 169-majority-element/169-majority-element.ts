b7c46e39a704c463cd6e4c6f4942f82c6970e6ce
Diogo Cardoso <diogocardoso92@outlook.com>
2022-08-14 17:19:36 +0100

    Time: 153 ms (8.78%), Space: 46.1 MB (43.41%) - LeetHub

--- 169-majority-element/169-majority-element.ts
+++ 169-majority-element/169-majority-element.ts
@@ -2,8 +2,6 @@ function majorityElement(nums: number[]): number {
     const m = new Map();
     for (let i = 0; i < nums.length; i++) {
         m.set(nums[i], m.get(nums[i]) + 1 || 1);
-    }
-    for (let i = 0; i < nums.length; i++) {
         if (m.get(nums[i]) > Math.floor(nums.length / 2)) {
             return nums[i];
         }
